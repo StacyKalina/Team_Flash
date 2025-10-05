e704beafdec021f77b2daf2c74a9f3ad4aeea710
Maria <abaturinamary@gmail.com>
2025-10-05 17:12:09 +0200

    Section Categories

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { MainBanner } from '../Components/MainBanner'
-
+import { Categories } from '../Components/Categories';
 export const Main = () => {
 return (
 <div>
         <MainBanner/>
+        <Categories />
 </div>
 );
 
