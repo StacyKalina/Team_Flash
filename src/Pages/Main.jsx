ec1529b09a8f67f2a0b6542113804d47f25e4ffd
Maryna <maryna@gamil.com>
2025-10-06 01:21:10 +0200

    create_Store

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -8,7 +8,6 @@ return (
 <div>
     <MainBanner/>
     <SectionCategories/>
-    
 </div>
 );
 
---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+i
+
+
+export const store = configureStore({
+    reducer: { // не бывает экшина, который бы обрабатывался неск редьюсерами
+
+    }
+
+
+})
