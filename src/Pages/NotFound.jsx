a3e2c64aaacc4814c0af4544b08bd3ff8cff1d5b
Vera <ilichovav@gmail.com>
2025-10-21 22:08:49 +0200

    NotFound Page navigation

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import styles from './NotFound.module.css';
-import Button  from "../Components/MainButton";
-// import { useNavigate } from "react-router-dom";
-// import { useEffect } from "react";
-// import { useLocation } from "react-router-dom";
-// import { useRef } from "react";
-<Button text="Go Home" onClick={() => alert('Go Home!')} />
+import Button from "../Components/MainButton";
+import { Link, NavLink } from 'react-router-dom'
 
 export const NotFound = () => {
     return (
-        <div className={styles.notFoundPageContent}>
-            <div className={styles.notFoundImgContent}/>
-                <div className={styles.notFoundTextContent}/>  
-            <Button text="Go Home" onClick={() => alert('Go Home!')} />    
-                
-        </div>
-        
+        <nav>
+            <div className={styles.notFoundPageContent}>
+                <div className={styles.notFoundImgContent} />
+                <div className={styles.notFoundTextContent} />
+
+
+                <NavLink to="/" className={({ isActive }) => isActive ? styles.active : ""}                >
+                    <Button text="Go Home" onClick={() => alert('Go Home!')} />
+                </NavLink>
+            </div>
+        </nav>
+
     )
 }
