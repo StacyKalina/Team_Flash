87057cf3ded89d3b5cd643f65d9fb1517fe84713
Vera <ilichovav@gmail.com>
2025-10-29 17:16:08 +0100

    Верстка модального окна Congratulations!

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,16 +1,17 @@
 import styles from "./Modal.module.css";
 import ReactDOM from "react-dom";
-import burger from "../Images/burger.png"
+import closeIcon from "../../Images/icons/ic-x.svg";
 export const Modal = ({ isOpen, onClose, children }) => {
-    if (!isOpen) return null;
+    if (!isOpen) {return null;}
+
 
     return ReactDOM.createPortal(
         <>
             <div className={styles.modalBackdrop}></div>
             <div className={styles.modalOverlay}>
                 <div className={styles.modalContent}>{children}</div>
-                <div className="iconWrapper">
-                    <img src={burger} alt="Close" onClick={onClose} />
+                <div className={styles.iconWrapper}>
+                  <img src={closeIcon} alt="Close" onClick={onClose} /> 
                 </div>
             </div>
 
