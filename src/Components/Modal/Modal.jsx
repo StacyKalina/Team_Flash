c8b33ceb1124d9a7f7792749061d55dfc826d700
Vera <ilichovav@gmail.com>
2025-11-02 16:23:04 +0100

    Модальное окно Successful Order адаптив

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,27 +1,7 @@
-// import styles from "./Modal.module.css";
-// import ReactDOM from "react-dom";
-// import closeIcon from "../../Images/icons/ic-x.svg";
-//  // let isModalOpen=false;
-// export const Modal = ({ isOpen, onClose, children }) => {
-//    if (!isOpen) {return null;}
 
-//     return ReactDOM.createPortal(
-//         <>
-//             <div className={styles.modalBackdrop}></div>
-//             <div className={styles.modalOverlay}>
-//                 <div className={styles.modalContent}>{children}</div>
-//                 <div className={styles.iconWrapper}>
-//                   <img src={closeIcon} alt="Close" onClick={onClose}/> 
-//                 </div>
-//             </div>
-
-//         </>,
-//         document.body
-//     );
-// }
 import styles from "./Modal.module.css";
 import ReactDOM from "react-dom";
-import closeIcon from "../../Images/icons/ic-x.svg";
+import closedIcon from "../../Images/icons/ic-xw.svg";
 
 export const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
@@ -42,11 +22,11 @@ export const Modal = ({ isOpen, onClose, children }) => {
         <button
           type="button"
           onClick={onClose}
-          className={styles.closeButton}
+           className={styles.closeButton}
           aria-label="Close modal"
         >
           <img
-            src={closeIcon}
+            src={closedIcon}
             alt="Close"
             className={styles.closeIcon}
           />
